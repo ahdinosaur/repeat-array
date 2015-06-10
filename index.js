4b8c908306936bda6eadf1a67744d4172946fb25
Michael Williams <dinosaur@riseup.net>
2015-06-11 01:21:22 +1200

    feature: can now repeat functions into arrays

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ module.exports = function (input, n) {
 
   // for each of n iterations
   for (var i = 0; i < n; i++) {
-    
-    // extend our result with input array
-    input.forEach(function (element) {
-      result.push(element);
-    })
-
+    //
+    //  extend our result with input
+    //
+    if (Array.isArray(input)) {
+      input.forEach(function (element) {
+        result.push(element)
+      })
+    } else if (typeof input == 'function') {
+      result.push(input())
+    } else {
+      throw new Error('input is not an array or function: ', input)
+    }
   }
 
-  // return result
-  return result;
+  return result
 }
--- test/index.js
+++ test/index.js
@@ -2,29 +2,32 @@ var expect = require('chai').expect;
 
 describe("#repeat-array", function () {
 
-  var repeat;
+  var repeat
 
   it("should load module", function () {
     repeat = require('../');
     expect(repeat).to.exist;
-  });
+  })
 
   it("should repeat array of 3 numbers 3 times", function () {
     expect(repeat([1,2,3],3))
     .to.deep.equal([1,2,3,1,2,3,1,2,3])
-    ;
-  });
+  })
 
   it("should repeat array of 6 strings 2 times", function () {
     expect(repeat(['ab','cd','ef','h','i','j'],2))
     .to.deep.equal(['ab','cd','ef','h','i','j','ab','cd','ef','h','i','j'])
-    ;
-  });
+  })
 
   it("should do 8 repeats of 1 item 2 times for total length of 256", function () {
     expect(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat([0],2),2),2),2),2),2),2),2),2)
     .to.have.length(256)
-    ;
-  });
+  })
 
-});
+  it("should do 8 repeats of a function", function () {
+    var i = 0
+    expect(repeat(function () { return i++ },8))
+    .to.deep.equal([0,1,2,3,4,5,6,7])
+  })
+
+})
