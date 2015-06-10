715ed0c6d9078b5feb40b3d8a8c7f1e6846c9391
Michael Williams <dinosaur@riseup.net>
2015-06-11 01:20:21 +1200

    check n

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 module.exports = function (input, n) {
 
   // start with empty array
-  var result = [];
+  var result = []
+
+  // check n
+  if (typeof n !== 'number') {
+    throw new Error('n is not a number: ', n)
+  }
 
   // for each of n iterations
   for (var i = 0; i < n; i++) {
